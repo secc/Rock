1851dddea5354a626780872092d921f2b655416a
GerhardPowell <gerhard@sparkdevnetwork.org>
2018-10-09 11:25:28 -0700

    + Registration Exception 'Given key not present in dictionary' (Fixes #3311)

--- RockWeb/Blocks/Event/RegistrationEntry.ascx.cs
+++ RockWeb/Blocks/Event/RegistrationEntry.ascx.cs
@@ -2667,7 +2667,7 @@ namespace RockWeb.Blocks.Event
                     familyId = RegistrationTemplate.RegistrantsSameFamily == RegistrantsSameFamily.Ask ?
                         multipleFamilyGroupIds[familyGuid] :
                         singleFamilyId.Value;
-                    PersonService.AddPersonToFamily( person, true, multipleFamilyGroupIds[familyGuid], familyRoleId, rockContext );
+                    PersonService.AddPersonToFamily( person, true, familyId.Value, familyRoleId, rockContext );
                 }
                 else
                 {
