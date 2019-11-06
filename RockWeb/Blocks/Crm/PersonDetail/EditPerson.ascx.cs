22f87d1b6959961e2db515208d4586af8d1496a2
Bob Brown <Bobbrown8462@gmail.com>
2019-11-06 13:32:14 -0500

    ROCK-3831  Corrected Edit phone number issue. Modified Edit Person block to allow multiple phone numbers per phone type.  Multiple phone types will now display on the screen. Modified numbers will remove the old number and display the updated number. Removed numbers will be the only number removed.

--- RockWeb/Blocks/Crm/PersonDetail/EditPerson.ascx.cs
+++ RockWeb/Blocks/Crm/PersonDetail/EditPerson.ascx.cs
@@ -330,6 +330,7 @@ namespace RockWeb.Blocks.Crm.PersonDetail
                         person.ConnectionStatusValueId = ddlConnectionStatus.SelectedValueAsInt();
 
                         var phoneNumberTypeIds = new List<int>();
+                        var phoneNumbersScreen = new List<PhoneNumber>();
 
                         bool smsSelected = false;
 
@@ -350,7 +351,8 @@ namespace RockWeb.Blocks.Crm.PersonDetail
                                     int phoneNumberTypeId;
                                     if ( int.TryParse( hfPhoneType.Value, out phoneNumberTypeId ) )
                                     {
-                                        var phoneNumber = person.PhoneNumbers.FirstOrDefault( n => n.NumberTypeValueId == phoneNumberTypeId );
+                                        var phoneNumberList = person.PhoneNumbers.Where( n => n.NumberTypeValueId == phoneNumberTypeId ).ToList();
+                                        var phoneNumber = phoneNumberList.FirstOrDefault( pn => pn.Number == PhoneNumber.CleanNumber( pnbPhone.Number ) );
                                         string oldPhoneNumber = string.Empty;
                                         if ( phoneNumber == null )
                                         {
@@ -383,11 +385,12 @@ namespace RockWeb.Blocks.Crm.PersonDetail
                             }
                         }
 
-                    // Remove any blank numbers
-                    var phoneNumberService = new PhoneNumberService( rockContext );
-                        foreach ( var phoneNumber in person.PhoneNumbers
-                            .Where( n => n.NumberTypeValueId.HasValue && !phoneNumberTypeIds.Contains( n.NumberTypeValueId.Value ) )
-                            .ToList() )
+                        // Remove any changed or removed numbers
+                        var phoneNumberService = new PhoneNumberService( rockContext );
+                        var phoneNumbersToRemove = person.PhoneNumbers
+                            .Where( n => !phoneNumbersScreen.Any( n2 => n2.Number == n.Number && n2.NumberTypeValueId == n.NumberTypeValueId ) ).ToList();
+
+                        foreach ( var phoneNumber in phoneNumbersToRemove )
                         {
                             person.PhoneNumbers.Remove( phoneNumber );
                             phoneNumberService.Delete( phoneNumber );
@@ -396,7 +399,7 @@ namespace RockWeb.Blocks.Crm.PersonDetail
                         person.Email = tbEmail.Text.Trim();
                         person.IsEmailActive = cbIsEmailActive.Checked;
                         person.EmailPreference = rblEmailPreference.SelectedValue.ConvertToEnum<EmailPreference>();
-                        person.CommunicationPreference = rblCommunicationPreference.SelectedValueAsEnum<CommunicationType>(); ;
+                        person.CommunicationPreference = rblCommunicationPreference.SelectedValueAsEnum<CommunicationType>(); 
                         person.GivingGroupId = ddlGivingGroup.SelectedValueAsId();
                         person.IsLockedAsChild = cbLockAsChild.Checked;
 
@@ -708,21 +711,28 @@ namespace RockWeb.Blocks.Crm.PersonDetail
             {
                 foreach ( var phoneNumberType in phoneNumberTypes.DefinedValues )
                 {
-                    var phoneNumber = Person.PhoneNumbers.FirstOrDefault( n => n.NumberTypeValueId == phoneNumberType.Id );
-                    if ( phoneNumber == null )
+                    var phoneNumberList = Person.PhoneNumbers.Where( n => n.NumberTypeValueId == phoneNumberType.Id ).ToList();
+                    if ( phoneNumberList.Count() == 0 )
                     {
-                        var numberType = new DefinedValue();
-                        numberType.Id = phoneNumberType.Id;
-                        numberType.Value = phoneNumberType.Value;
+                        phoneNumberList.Add( null );
+                    }
+                    foreach ( var phoneNumberTemp in phoneNumberList )
+                    {
+                        var phoneNumber = phoneNumberTemp;
+                        if ( phoneNumber == null )
+                        {
+                            var numberType = new DefinedValue();
+                            numberType.Id = phoneNumberType.Id;
+                            numberType.Value = phoneNumberType.Value;
 
-                        phoneNumber = new PhoneNumber { NumberTypeValueId = numberType.Id, NumberTypeValue = numberType };
-                        phoneNumber.IsMessagingEnabled = mobilePhoneType != null && phoneNumberType.Id == mobilePhoneType.Id;
-                    }
-                    else
-                    {
-                        // Update number format, just in case it wasn't saved correctly
-                        phoneNumber.NumberFormatted = PhoneNumber.FormattedNumber( phoneNumber.CountryCode, phoneNumber.Number );
-                    }
+                            phoneNumber = new PhoneNumber { NumberTypeValueId = numberType.Id, NumberTypeValue = numberType };
+                            phoneNumber.IsMessagingEnabled = mobilePhoneType != null && phoneNumberType.Id == mobilePhoneType.Id;
+                        }
+                        else
+                        {
+                            // Update number format, just in case it wasn't saved correctly
+                            phoneNumber.NumberFormatted = PhoneNumber.FormattedNumber( phoneNumber.CountryCode, phoneNumber.Number );
+                        }
 
                     phoneNumbers.Add( phoneNumber );
                 }
