56e7676bca0a30aebc66f2e2add746f43df43c12
Mark Lee <mlee@secc.org>
2020-01-07 16:25:45 -0500

    ROCK-4131 Fixed duplicate meta tags

--- Rock/Lava/RockFilters.cs
+++ Rock/Lava/RockFilters.cs
@@ -3786,7 +3786,7 @@ namespace Rock.Lava
                 HtmlMeta metaTag = new HtmlMeta();
                 metaTag.Attributes.Add( attributeName, attributeValue );
                 metaTag.Content = input;
-                page.Header.Controls.Add( metaTag );
+                page.AddMetaTag( metaTag );
             }
 
             return null;
